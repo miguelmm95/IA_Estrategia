978484cb3e279563611062ea8a5ede5665d95ab4
al385752 <101199100+al385752@users.noreply.github.com>
2022-11-20 12:04:30 +0100

    unitsScripts
    creados los scripts para las tres clases de unidades de combate, en cada uno se declaran las variables básicas de cada uno: salud máxima, velocidad (saltos de casilla máximos) y rango de ataque

---
+++ Scripts/Combat Units/heavy.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heavy : MonoBehaviour
+{
+    private int health = 500;
+    private int speed = 15;
+    private int attackRange = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Scripts/Combat Units/ranged.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ranged : MonoBehaviour
+{
+    private int health = 100;
+    private int speed = 5;
+    private int attackRange = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
