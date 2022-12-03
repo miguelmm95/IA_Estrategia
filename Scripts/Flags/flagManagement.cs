6572739315c1c2655ee62dcaa224a4bdbb5d7e76
Dastdjc <90397187+Dastdjc@users.noreply.github.com>
2022-12-03 18:04:11 +0100

    Flag Management

--- Scripts/Flags/flagManagement.cs
+++ Scripts/Flags/flagManagement.cs
@@ -7,9 +7,8 @@ public class flagManagement : MonoBehaviour
     private GameObject gm;
     private Grid gridTiles;
     private List<Tile> neighbours;
-    private GameObject unit;
+    private BaseUnit unit;
     private int turno;
-    private bool capturado;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +16,6 @@ public class flagManagement : MonoBehaviour
         GameObject gm = GameObject.FindWithTag("Grid");
         gridTiles = gm.GetComponent<Grid>();
         neighbours = gridTiles.GetNeighboursUnit(this.transform.parent.GetComponent<Tile>());
-        
     }
 
     // Update is called once per frame
@@ -27,22 +25,33 @@ public class flagManagement : MonoBehaviour
         {
             if (n.unitStay)
             {
-                capturado = true;
-                unit = n.unit; 
+                unit = n.unit.GetComponent<BaseUnit>();
+                break;
+            }
+            else
+            {
+                unit = null;
             }
         }
 
         switch (this.tag)
         {
             case "Bandera_Jugador":
-                if (turno == 3)
+                if (turno == 3 && unit.player == Player.AI)
                 {
-
+                    this.transform.parent.GetComponent<Tile>().DisableBanderaJugador();
+                    this.transform.parent.GetComponent<Tile>().SetBanderaIA();
+                    turno = 0;
                 }
                 break;
 
             case "Bandera_IA":
-
+                if (turno == 3 && unit.player == Player.Human)
+                {
+                    this.transform.parent.GetComponent<Tile>().DisableBanderaIA();
+                    this.transform.parent.GetComponent<Tile>().SetBanderaJugador();
+                    turno = 0;
+                }
                 break;
 
             default:
